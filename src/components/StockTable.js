67882aaf3364b0f853bc1c0d42afc0a4108eae89
brownviper <s4saz@hotmail.com>
2019-03-26 07:11:50 +0000

    StockTable test is now passing

---
+++ src/components/StockTable.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class StockTable extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Stock Table</h2>
+            </div>
+        );
+    }
+}
+
+export default StockTable;
