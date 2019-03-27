e5da4a7b7275ff3aee573304bfdf946961388376
brownviper <s4saz@hotmail.com>
2019-03-27 08:37:05 +0000

    new traders are shown in the TradersTable

--- src/components/TradersTable.js
+++ src/components/TradersTable.js
@@ -1,46 +1,58 @@
+// @flow
+
 import React, { Component } from 'react';
 import {Table} from 'reactstrap';
+import { connect } from 'react-redux';
+import type { State } from '../types';
 
-class TradersTable extends Component {
+export type Props = {
+    traders: Traders
+}
+class TradersTable extends Component<Props> {
+
+    props = {
+        traders: []
+    };
+
     render() {
         return (
             <div>
-            <h2>Traders Table</h2>
-            <div>
-            <Table striped>
-                <thead>
-                    <tr>
-                        <th />
-                        <th>Symbol</th>
-                        <th>Share price</th>
-                        <th># shares</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                </tbody>
-            </Table>
-            </div>
+                <h3>Traders Table</h3>
+                <div>
+                    <Table striped>
+                        <thead>
+                            <tr>
+                                <th />
+                                <th>Symbol</th>
+                                <th>Share price</th>
+                                <th># shares</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.props.traders.map(trader => {
+                                return (
+                                    <tr>
+                                        <th />
+                                        <td>{trader.symbol}</td>
+                                        <td>{trader.price}</td>
+                                        <td>{trader.count}</td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </Table>
+                </div>
             </div>
         );
     }
 }
 
-export default TradersTable;
+const mapStateToProps = (state: State) => {
+    return state;
+};
+
+const connector = connect(
+    mapStateToProps
+);
+
+export default connector(TradersTable);
