231d7e3fb7d1b5fed957fe14f9f030d951e058bc
brownviper <s4saz@hotmail.com>
2019-03-27 14:27:30 +0000

    added DividendYield based on a fixed value = 7.08

--- src/processors/TraderProcessor.js
+++ src/processors/TraderProcessor.js
@@ -9,8 +9,46 @@ class TraderProcessor {
   this.traders = traders;
  }
 
- calculateDividendYield() {
-  return this.traders;
+ arrangeTraders() {
+  return this.getUniqueSymbols().map(symbol => {
+   const filtered = this.traders.filter(trader => trader.symbol === symbol);
+       return {
+        symbol: filtered[0].symbol,
+        traders: filtered
+       };
+  });
+ }
+
+ getUniqueSymbols() {
+  var uniqueSymbols = [];
+
+  const res = this.traders.map(trader => {
+   if (uniqueSymbols.includes(trader.symbol)) return;
+   else uniqueSymbols.push(trader.symbol);
+  });
+  return uniqueSymbols;
+ }
+
+ calculateStockExchangeParams() {
+  arrangeTraders.map(tradeCollection => {
+   return {
+    symbol: item.symbol,
+    dividendYield: this.calculateDividendYield(7.08, tradeCollection),
+    peRatio: 1.0,
+    geometricMean: 1.0,
+    volumeWeighted: 1.0
+   }
+  });
+ }
+
+ static calculateDividendYield(dividend: number, collection: Traders) {
+
+  const sortedTraders = collection.sort((a, b) => {
+   return new Date(b.timeStamp) - new Date(a.timeStamp)
+  });
+
+  return (dividend / parseFloat(sortedTraders[0].price) * 100.00).toPrecision(2);
+
  }
 }
 
--- src/processors/TraderProcessor.test.js
+++ src/processors/TraderProcessor.test.js
@@ -4,19 +4,108 @@ import TradeProcessor from './TraderProcessor';
 import {Traders} from '../types/traders';
 
 describe('trade stock processor', () => {
-    it('should calculate the dividend yield', () => {
+    it('should extract an array that contain unique symbols', () => {
         const traders: Traders = [
             {
                 id: '',
-                symbol: '',
+                symbol: 'aaa',
                 price: '',
                 count: '',
                 timeStamp: ''
-            }
+            },            {
+                id: '',
+                symbol: 'bbb',
+                price: '',
+                count: '',
+                timeStamp: ''
+            },
+            {
+                id: '',
+                symbol: 'aaa',
+                price: '',
+                count: '',
+                timeStamp: ''
+            },
+            {
+                id: '',
+                symbol: 'bbb',
+                price: '',
+                count: '',
+                timeStamp: ''
+            },
+
         ];
 
         const processor: TradeProcessor = new TradeProcessor(traders);
 
-        expect(processor.calculateDividendYield()).toEqual(traders);
+        expect(processor.getUniqueSymbols()).toEqual(['aaa', 'bbb']);
+    });
+
+    it('should arrange the traders based on Symbol', () => {
+        const traders: Traders = [
+            {
+                id: '',
+                symbol: 'aaa',
+                price: '',
+                count: '',
+                timeStamp: ''
+            },            {
+                id: '',
+                symbol: 'bbb',
+                price: '',
+                count: '',
+                timeStamp: ''
+            },
+            {
+                id: '',
+                symbol: 'aaa',
+                price: '',
+                count: '',
+                timeStamp: ''
+            },
+            {
+                id: '',
+                symbol: 'bbb',
+                price: '',
+                count: '',
+                timeStamp: ''
+            },
+
+        ];
+
+        const processor: TradeProcessor = new TradeProcessor(traders);
+        const expectedArrangedTraders = [
+            {
+                symbol: 'aaa',
+                traders: [traders[0], traders[2]]
+            },
+            {
+                symbol: 'bbb',
+                traders: [traders[1], traders[3]]
+            }
+        ];
+
+        expect(processor.arrangeTraders()).toEqual(expectedArrangedTraders);
+    });
+
+    it('should calculate the dividend yield given traders and ', () => {
+        const traders: Traders = [
+            {
+                id: '',
+                symbol: '',
+                price: '10.00',
+                count: '',
+                timeStamp: 'Wed Mar 27 2019 14:09:24 GMT+0000 (Greenwich Mean Time)'
+            },
+            {
+                id: '',
+                symbol: '',
+                price: '45.00',
+                count: '',
+                timeStamp: 'Wed Mar 28 2019 14:09:24 GMT+0000 (Greenwich Mean Time)'
+            }
+        ];
+
+        expect(TradeProcessor.calculateDividendYield(1.2, traders)).toEqual("2.7");
     });
 });
